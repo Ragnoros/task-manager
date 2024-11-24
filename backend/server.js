8df53756509cb03f054bc895299a14952a2cac62
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-11-24 13:12:39 +0000

    general setup still

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+const app = express();
+
+dotenv.config();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("server running on ", PORT);
+});
