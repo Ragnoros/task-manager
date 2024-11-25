9871cb5d0859a6e6dd3474a9a5dfb2c9ea683c1f
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-11-25 16:05:46 +0000

    started the task model

---
+++ backend/models/task.model.js
@@ -0,0 +1,19 @@
+import mongoose, { SchemaType } from "mongoose";
+const { Schema } = mongoose;
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
