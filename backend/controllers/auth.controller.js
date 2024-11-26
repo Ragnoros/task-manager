0c5fb183871397c87846bcc8274bad7df49897e7
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-11-26 20:05:11 +0000

    Completed the signup endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,29 @@
-export const signup = (req, res) => {
-  console.log(req.body);
-  const { username, password, gender, profilePic } = req.body;
-  res.status(200).send(req.body);
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, password, confirmPassword, gender } = req.body;
+  try {
+    if (password !== confirmPassword) {
+      return res.status(400).json("Password and Confirm Password do not match");
+    }
+    const findUser = await User.findOne({ username });
+    if (findUser) {
+      return res.status(400).json("Username Already Exists");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const insertUser = await new User({
+      username,
+      password: hashedPassword,
+      gender,
+    });
+    await insertUser.save();
+    res.status(201).send(insertUser);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import tasksRoutes from "./routes/tasks.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
 
 dotenv.config();
@@ -13,5 +14,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", tasksRoutes);
 
 app.listen(PORT, () => {
+  connectToMongoDB();
   console.log("server running on ", PORT);
 });
