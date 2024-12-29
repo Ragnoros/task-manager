3c1d6afc309597228bfd0a845772cdc57b7c9a6b
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-12-29 20:03:45 +0000

    added a little more to the endpoint

--- backend/controllers/tasks.controller.js
+++ backend/controllers/tasks.controller.js
@@ -1,3 +1,9 @@
 export const postTask = (req, res) => {
+  const { title, description, userId, status } = req.body;
+
+  if (!title) {
+    return res.status(400).json("Please Enter a Title");
+  }
+
   res.status(200).send(req.body);
 };
