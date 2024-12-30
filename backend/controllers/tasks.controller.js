730fd32e0594c5ce50d624ea994cf03ee296ab5e
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-12-30 20:55:20 +0000

    almost finished create task endpoint

--- backend/controllers/tasks.controller.js
+++ backend/controllers/tasks.controller.js
@@ -1,9 +1,29 @@
-export const postTask = (req, res) => {
-  const { title, description, userId, status } = req.body;
+import Task from "../models/task.model.js";
 
-  if (!title) {
-    return res.status(400).json("Please Enter a Title");
+export const postTask = async (req, res) => {
+  try {
+    const { title, description, userId, status } = req.body;
+
+    if (!title) {
+      return res.status(400).json("Please Enter a Title");
+    }
+
+    const findTask = await Task.findOne({ title });
+    console.log(findTask);
+    if (findTask && findTask.userId == userId) {
+      return res.status(400).json("Task already exists");
+    }
+
+    const insertTask = await new Task({
+      title,
+      userId,
+      description,
+      status,
+    });
+    await insertTask.save();
+    res.status(201).send(insertTask);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json("Error Internal Server Error");
   }
-
-  res.status(200).send(req.body);
 };
