5dd13c378b37a61dabd052aa8074a0c839afadb5
Lorontal <137430073+Lorontal@users.noreply.github.com>
2025-01-12 19:06:31 +0000

    added a delete task endpoint

--- backend/routes/tasks.routes.js
+++ backend/routes/tasks.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { getTasks, postTask } from "../controllers/tasks.controller.js";
+import {
+  getTasks,
+  postTask,
+  deleteTask,
+} from "../controllers/tasks.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.post("/:userId/create", protectRoute, postTask);
 router.get("/:userId", protectRoute, getTasks);
+router.delete("/:userId/delete/:taskId", protectRoute, deleteTask);
 
 export default router;
